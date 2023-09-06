79c17d47e88b1e67622a7b11d44512540a6ab8e0
jordan <yardengonen4@gmail.com>
2023-09-06 11:54:31 +0300

    find_nearest_integer function was added

--- Desktop/yarden/GitExercise/Program.py
+++ Desktop/yarden/GitExercise/Program.py
@@ -1,5 +1,11 @@
-
+import math 
 total_sum = 0
 for number in range(1, 101):
     total_sum += number
 print(total_sum)
+def find_nearest_integer(number):
+    return round(number)
+print(find_nearest_integer(5))
+print(find_nearest_integer(6))
+print(find_nearest_integer(7))
+print(find_nearest_integer(8))
